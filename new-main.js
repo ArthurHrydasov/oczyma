f7a0d4d95c7f5ca0a1f31e16ef04cc5d3af77edd
ArthurHrydasov <69531159+ArthurHrydasov@users.noreply.github.com>
2024-05-04 19:23:26 +0300

    Update new-main.js

--- new-main.js
+++ new-main.js
@@ -68,6 +68,49 @@ if (window.innerWidth < 991) {
   });
 }
 
+  let customEase = "M0,0,C0,0,0.13,0.34,0.238,0.442,0.305,0.506,0.322,0.514,0.396,0.54,0.478,0.568,0.468,0.56,0.522,0.584,0.572,0.606,0.61,0.719,0.714,0.826,0.798,0.912,1,1,1,1";
+  let counter = {
+    value: 10,
+  };
+  let loaderDuration = 5;
+  if (sessionStorage.getItem("visited") !== null) {
+    loaderDuration = 1;
+    counter = {
+      value: 10,
+    };
+  }
+  sessionStorage.setItem("visited", "true");
+
+  function updateLoaderText() {
+    let progress = Math.round(counter.value);
+    $(".percent").text(progress);
+  }
+  function endLoaderAnimation() {
+    $(".loader").addClass("load_none");
+    setTimeout(() => {
+      $("body").css("overflow", "auto");
+    }, 870);
+  }
+
+  let tl = gsap.timeline({
+    onComplete: endLoaderAnimation,
+  });
+  tl.to(counter, {
+    value: 100,
+    onUpdate: updateLoaderText,
+    duration: loaderDuration,
+    ease: CustomEase.create("custom", customEase),
+  });
+  tl.to(
+    ".preload_logo_overlay",
+    {
+      height: "100%",
+      duration: loaderDuration,
+      ease: CustomEase.create("custom", customEase),
+    },
+    0
+  );
+
 
 
 
